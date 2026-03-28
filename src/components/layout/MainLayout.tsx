9b96ea352d64c4820a1ed75c4106a37bc4cc7350
AdityaWisnuu <adtywisnuwardana003@gmail.com>
2026-03-28 16:57:01 +0700

    feat(dashboard): full-height layout, progress bars, quick actions, richer activity

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -9,9 +9,9 @@ export default function MainLayout() {
   return (
     <div className="min-h-screen bg-background">
       <Navbar onToggleSidebar={() => setSidebarOpen((o) => !o)} />
-      <div className="flex pt-16">
+      <div className="flex pt-16 min-h-[calc(100vh-4rem)]">
         <Sidebar open={sidebarOpen} onClose={() => setSidebarOpen(false)} />
-        <main className="flex-1 p-4 lg:p-6">
+        <main className="flex-1 p-4 lg:p-6 min-h-full">
           <Outlet />
         </main>
       </div>
